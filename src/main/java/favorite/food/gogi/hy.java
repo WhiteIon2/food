d45a959a757456c4179ca834ae19c8e78957bfc6
hyunji <roseloren@naver.com>
2022-01-20 20:04:41 +0900

    dddd

--- src/main/java/favorite/food/gogi/hy.java
+++ src/main/java/favorite/food/gogi/hy.java
@@ -3,7 +3,7 @@ package favorite.food.gogi;
 public class hy {
 
 	public static void main(String[] args) {
-		System.out.println("연습용입니다.11111111");
+		System.out.println("연습용입니다.111ㅇㄴㄹㄴ11111");
 		
 	}
 }
